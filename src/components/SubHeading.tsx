47c6e1920aa5b07c47f7ab4b31024ecc253f1bdc
codermjurul <133157830+codermjurul@users.noreply.github.com>
2025-09-01 16:16:18 +0600

    Updated SubHeading.tsx

--- src/components/SubHeading.tsx
+++ src/components/SubHeading.tsx
@@ -8,7 +8,8 @@ interface SubHeadingProps {
 export function SubHeading({ children, className = '' }: SubHeadingProps) {
   // All sub-heading styles are defined here.
   // We use 'font-lato' for the Lato font.
-  const baseClasses = "font-lato font-semibold text-text-heading";
+  // The color is now set to a navy blue (text-blue-900).
+  const baseClasses = "font-lato font-semibold text-blue-900";
 
   return (
     <h2 className={`${baseClasses} ${className}`}>
