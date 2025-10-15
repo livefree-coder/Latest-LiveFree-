21d2f82f3691a884683fee520e0387c3c9470964
livefree-coder <livefree@livefreeimmigration.com>
2025-10-15 14:01:36 +0600

    Updated index.css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -50,11 +50,11 @@ const Header: React.FC = () => {
     <header className="bg-white shadow-sm sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-full">
         <div className="flex justify-between items-center h-full">
-          <a href="/" className="site-logo py-1">
+          <a href="/" className="flex items-center">
             <img
-              src="/Artboard 1 (1).svg"
-              alt="LiveFree Immigration Canada Logo"
-              className="w-auto"
+              src="/logo2.svg"
+              alt="LiveFree Immigration Canada"
+              className="h-12 w-auto"
             />
           </a>
 
