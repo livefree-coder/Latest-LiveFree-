152cd0df3db1ae50a1293dc979a63cc4bf1c8fc6
livefree-coder <livefree@livefreeimmigration.com>
2025-10-15 20:18:28 +0600

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -47,7 +47,7 @@ const Header: React.FC = () => {
 
 
   return (
-    <header className="bg-white shadow-sm sticky top-0 z-50 h-36">
+    <header className="bg-white shadow-sm sticky top-0 z-50 h-40">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-full">
         <div className="flex justify-between items-center h-full">
           <a href="/" className="flex items-center">
