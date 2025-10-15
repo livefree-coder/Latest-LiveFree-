d2b79089eb62596a24afa567b0079edb9390a87a
livefree-coder <livefree@livefreeimmigration.com>
2025-10-15 14:05:14 +0600

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -54,7 +54,7 @@ const Header: React.FC = () => {
             <img
               src="/logo2.svg"
               alt="LiveFree Immigration Canada"
-              className="h-28 w-auto"
+              className="h-32 w-auto"
               style={{ imageRendering: '-webkit-optimize-contrast', filter: 'contrast(1.15) brightness(1.05) saturate(1.1)' }}
             />
           </a>
